751b1a2c98a43e44c5a10d4f67f2dfaf0111b2da
ilavine <70871601+ilavine@users.noreply.github.com>
2020-11-30 12:48:56 -0500

    Update Fixer.py

--- Devel/Final/Fixer.py
+++ Devel/Final/Fixer.py
@@ -2,7 +2,7 @@
 Project Name:  Final CIS 210 M
 Program Name:  Fixer.py
 Date:          10 November 2020
-Synopsis:      This program removes the carage return line feed
+Synopsis:      This program removes the carriage return line 
 Written by:    Iuliia Lavine
        
 '''
