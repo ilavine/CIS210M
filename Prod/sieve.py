0d7edb82f4372d36654a3f24c49441882e63a1d8
Iuliia Lavine <iLavine073@students.ccsnh.ede>
2020-12-02 07:42:27 -0500

    uploaded sive + stack

---
+++ Prod/sieve.py
@@ -0,0 +1,55 @@
+"""
+Program Name:  sieve.py
+Writen by:     Iuliia Lavine
+Date:          10 November 2020
+Synopsis:      This is the implementation of Sieve_of_Eratosthenes
+
+Helpful links: https://www.youtube.com/watch?v=pKvGYOnO9Ao
+
+       
+"""
+
+from simple_stack import Stack
+import random 
+
+def sieve(n):
+	for i in n:
+		for j in n:
+			if j != i and j%i == 0:
+				n.remove(j)
+	return n
+
+
+def isPrime(numero):
+	for i in range(2, numero):
+		if numero % i == 0:
+			return False
+	return True
+
+
+
+print ("Is Prime?\n")
+print ('Is 2 a prime?', isPrime(2))
+print ('Is 3 a prime?', isPrime(3))
+print ('Is 4 a prime?', isPrime(4))
+print ('Is 5 a prime?', isPrime(5))
+print ('Is 10 a prime?', isPrime(10))
+
+stack = Stack()
+data = [random.randint(1,101) for _ in range(100)]
+stack.push(data)
+print("\nPushing to Stack\n\n",data)
+
+print ("\nSieve of Eratosthenes\n")
+numbers = data
+print(*data, sep=' ')
+print ("\nOutput (primes):\n")
+primes = sieve(data)
+print(*primes, sep=' ')
+stack.push(primes)
+print("\nPushing to Stack\n\n", primes)
+
+howBig = stack.size()
+print('\n\n', howBig)
+
+
